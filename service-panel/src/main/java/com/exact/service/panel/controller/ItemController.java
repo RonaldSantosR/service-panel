a05e48b9d32818074379d72240fa88c77d9ad77c
usuario <ymamani@exact.com.pe>
2019-08-16 12:10:37 -0500

    commit --m "subir listar item"

---
+++ service-panel/src/main/java/com/exact/service/panel/controller/ItemController.java
@@ -0,0 +1,25 @@
+package com.exact.service.panel.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.exact.service.panel.entity.Item;
+import com.exact.service.panel.service.interfaces.IItemservice;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+	
+	@Autowired
+	IItemservice itemservice;
+	
+	@GetMapping()
+	public ResponseEntity<Iterable<Item>> listarItem() {
+		return  new ResponseEntity<Iterable<Item>>(itemservice.listaritem(),HttpStatus.OK);
+		//return new ResponseEntity<String>(perfilservice.obtenerCorreo(id),HttpStatus.OK);
+	}
+}
