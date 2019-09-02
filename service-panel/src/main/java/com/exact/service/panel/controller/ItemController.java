a94ebbfa38752c84ac8244d1a982c8e7eb9340fd
usuario <ymamani@exact.com.pe>
2019-09-02 15:48:42 -0500

    subir cambios de modificar y agregar

--- service-panel/src/main/java/com/exact/service/panel/controller/ItemController.java
+++ service-panel/src/main/java/com/exact/service/panel/controller/ItemController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,8 +43,13 @@ public class ItemController {
 	
 	@PutMapping("/modificar")
 	public ResponseEntity<Item> modificarItem(@RequestBody Item item){
-		return new ResponseEntity<>(itemservice.modificarItem(item),HttpStatus.OK);
+		return new ResponseEntity<Item>(itemservice.modificarItem(item),HttpStatus.OK);
 	}
+	
+	@PostMapping()
+	public ResponseEntity<Item> guadarItem(@RequestBody Item item) {
+		return  new ResponseEntity<Item>(itemservice.agregarItem(item),HttpStatus.OK);
+	}	
 	
 	
 }
